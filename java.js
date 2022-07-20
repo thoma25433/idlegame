e55f54a0018c02f33ae9de56691b1e01e480bef7
Tommy Blount <102384381+thoma25433@users.noreply.github.com>
2022-07-20 19:25:02 -0400

    small changes - posting before break

--- java.js
+++ java.js
@@ -1,14 +1,30 @@
 var point = 0;
 var pointPerSecond = 0;
-var auto1Cost = 0;
-var auto1s = 0;
+var gen1Cost = 0;
+var gen1s = 0;
+var gen2Cost = 500;
+var gen2s = 0;
+var gen3Cost = 3000;
+var gen3s = 0;
+var prestigeLevel = 0;
+var prestigeCost = 10000;
+
+
+
+function prestige() {
+
+}
 
 function save() {
   var save = {
     point: point,
-    auto1s: auto1s,
-    auto1Cost: auto1Cost,
+    gen1s: gen1s,
+    gen1Cost: gen1Cost,
     pointPerSecond: pointPerSecond,
+    gen2Cost: gen2Cost,
+    gen2s: gen2s,
+    gen3Cost: gen3Cost,
+    gen3s: gen3s,
   }
   localStorage.setItem("save",JSON.stringify(save));
 }
@@ -16,9 +32,13 @@ function save() {
 function load() {
   var savegame = JSON.parse(localStorage.getItem("save"));
   if (typeof savegame.point !== "undefined") point = savegame.point;
-  if (typeof savegame.auto1s !== "undefined") auto1s = savegame.auto1s;
-  if (typeof savegame.auto1Cost !== "undefined") auto1Cost = savegame.auto1Cost;
   if (typeof savegame.pointPerSecond !== "undefined") pointPerSecond = savegame.pointPerSecond;
+  if (typeof savegame.gen1s !== "undefined") gen1s = savegame.gen1s;
+  if (typeof savegame.gen1Cost !== "undefined") gen1Cost = savegame.gen1Cost;
+  if (typeof savegame.gen2s !== "undefined") gen2s = savegame.gen2s;
+  if (typeof savegame.gen2Cost !== "undefined") gen2Cost = savegame.gen2Cost;
+  if (typeof savegame.gen3s !== "undefined") gen3s = savegame.gen3s;
+  if (typeof savegame.gen3Cost !== "undefined") gen3Cost = savegame.gen3Cost;
 }
 
 function delSave() {
@@ -28,23 +48,53 @@ function delSave() {
 setInterval(function() {
   point += pointPerSecond;
   document.getElementById("showPoint").innerText = point;
-  document.getElementById("auto1num").innerText = auto1s;
-  document.getElementById("auto1price").innerText = auto1Cost;
+  document.getElementById("gen1num").innerText = gen1s;
+  document.getElementById("gen1price").innerText = gen1Cost;
   document.getElementById("showpsec").innerText = pointPerSecond;
   // save()
 }, 1000)
 
-function buyAuto1() {
-  if (point >= auto1Cost) {
-    point -= auto1Cost;
+function buygen1() {
+  if (point >= gen1Cost) {
+    point -= gen1Cost;
     document.getElementById("showPoint").innerText = point;
-    auto1s++;
-    document.getElementById("auto1num").innerText = auto1s;
-    auto1Cost += 1;
-    auto1Cost *= 1.2;
-    auto1Cost = Math.round(auto1Cost);
-    document.getElementById("auto1price").innerText = auto1Cost;
+    gen1s++;
+    document.getElementById("gen1num").innerText = gen1s;
+    gen1Cost += 1;
+    gen1Cost *= 1.5;
+    gen1Cost = Math.round(gen1Cost);
+    document.getElementById("gen1price").innerText = gen1Cost;
     pointPerSecond += 1;
+    document.getElementById("showpsec").innerText = pointPerSecond;
+  } else {
+    alert("You don't have enough points!")
+  }
+}
+function buygen2() {
+  if (point >= gen2Cost) {
+    point -= gen2Cost;
+    document.getElementById("showPoint").innerText = point;
+    gen2s++;
+    document.getElementById("gen2num").innerText = gen2s;
+    gen2Cost *= 1.2;
+    gen2Cost = Math.round(gen2Cost);
+    document.getElementById("gen2price").innerText = gen2Cost;
+    pointPerSecond += 3;
+    document.getElementById("showpsec").innerText = pointPerSecond;
+  } else {
+    alert("You don't have enough points!")
+  }
+}
+function buygen3() {
+  if (point >= gen3Cost) {
+    point -= gen3Cost;
+    document.getElementById("showPoint").innerText = point;
+    gen3s++;
+    document.getElementById("gen3num").innerText = gen3s;
+    gen3Cost *= 1.2;
+    gen3Cost = Math.round(gen3Cost);
+    document.getElementById("gen3price").innerText = gen3Cost;
+    pointPerSecond += 7;
     document.getElementById("showpsec").innerText = pointPerSecond;
   } else {
     alert("You don't have enough points!")
