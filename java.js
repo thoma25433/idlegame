a15467609b79068c53f70c2e0ed13a76479b91e3
Tommy Blount <102384381+thoma25433@users.noreply.github.com>
2022-07-22 10:56:52 -0400

    completed prestige

--- java.js
+++ java.js
@@ -2,14 +2,15 @@ var point = 0;
 var pointPerSecond = 0;
 var gen1Cost = 0;
 var gen1s = 0;
+var gen1gain = 1;
 var gen2Cost = 500;
 var gen2s = 0;
+var gen2gain = 1;
 var gen3Cost = 3000;
 var gen3s = 0;
+var gen3gain = 1;
 var prestigeLevel = 0;
 var prestigeCost = 10;
-
-
 
 function prestige() {
   if (point >= prestigeCost) {
@@ -27,6 +28,12 @@ function prestige() {
     prestigeCost *= 2;
     prestigeCost = Math.round(prestigeCost);
     document.getElementById("prescost").innerText = prestigeCost;
+    gen1gain *= 1.5;
+    gen1gain = Math.round(gen1gain)
+    gen2gain *= 1.5;
+    gen2gain = Math.round(gen2gain)
+    gen3gain *= 1.5;
+    gen3gain = Math.round(gen3gain)
   } else {
     alert("You don't have enough points!")
   }
@@ -37,12 +44,19 @@ function save() {
     point: point,
     gen1s: gen1s,
     gen1Cost: gen1Cost,
+    gen1gain: gen1gain,
     pointPerSecond: pointPerSecond,
     gen2Cost: gen2Cost,
     gen2s: gen2s,
+    gen2gain: gen2gain,
     gen3Cost: gen3Cost,
     gen3s: gen3s,
+    gen3gain: gen3gain,
+    prestigeLevel: prestigeLevel,
+    prestigeCost: prestigeCost,
   }
+//   var dataToWrite = "Hello World!";
+// saveAs("./saves/data.txt", dataToWrite);
   localStorage.setItem("save",JSON.stringify(save));
 }
 
@@ -52,10 +66,15 @@ function load() {
   if (typeof savegame.pointPerSecond !== "undefined") pointPerSecond = savegame.pointPerSecond;
   if (typeof savegame.gen1s !== "undefined") gen1s = savegame.gen1s;
   if (typeof savegame.gen1Cost !== "undefined") gen1Cost = savegame.gen1Cost;
+  if (typeof savegame.gen1gain !== "undefined") gen1gain = savegame.gen1gain;
   if (typeof savegame.gen2s !== "undefined") gen2s = savegame.gen2s;
   if (typeof savegame.gen2Cost !== "undefined") gen2Cost = savegame.gen2Cost;
+  if (typeof savegame.gen2gain !== "undefined") gen2gain = savegame.gen2gain;
   if (typeof savegame.gen3s !== "undefined") gen3s = savegame.gen3s;
   if (typeof savegame.gen3Cost !== "undefined") gen3Cost = savegame.gen3Cost;
+  if (typeof savegame.gen3gain !== "undefined") gen3gain = savegame.gen3gain;
+  if (typeof savegame.prestigeLevel !== "undefined") prestigeLevel = savegame.prestigeLevel;
+  if (typeof savegame.prestigeCost !== "undefined") prestigeCost = savegame.prestigeCost;
 }
 
 function delSave() {
@@ -67,7 +86,16 @@ setInterval(function() {
   document.getElementById("showPoint").innerText = point;
   document.getElementById("gen1num").innerText = gen1s;
   document.getElementById("gen1price").innerText = gen1Cost;
+  document.getElementById("gen1add").innerText = gen1gain;
+  document.getElementById("gen2num").innerText = gen2s;
+  document.getElementById("gen2price").innerText = gen2Cost;
+  document.getElementById("gen2add").innerText = gen2gain;
+  document.getElementById("gen3num").innerText = gen3s;
+  document.getElementById("gen3price").innerText = gen3Cost;
+  document.getElementById("gen3add").innerText = gen3gain;
   document.getElementById("showpsec").innerText = pointPerSecond;
+  document.getElementById("showplvl").innerText = prestigeLevel;
+  document.getElementById("prescost").innerText = prestigeCost;
   // save()
 }, 1000)
 
@@ -81,8 +109,9 @@ function buygen1() {
     gen1Cost *= 1.5;
     gen1Cost = Math.round(gen1Cost);
     document.getElementById("gen1price").innerText = gen1Cost;
-    pointPerSecond += 1;
+    pointPerSecond += gen1gain;
     document.getElementById("showpsec").innerText = pointPerSecond;
+    document.getElementById("gen1add").innerText = gen1gain;
   } else {
     alert("You don't have enough points!")
   }
@@ -96,8 +125,9 @@ function buygen2() {
     gen2Cost *= 1.2;
     gen2Cost = Math.round(gen2Cost);
     document.getElementById("gen2price").innerText = gen2Cost;
-    pointPerSecond += 3;
+    pointPerSecond += gen2gain;
     document.getElementById("showpsec").innerText = pointPerSecond;
+    document.getElementById("gen2add").innerText = gen2gain;
   } else {
     alert("You don't have enough points!")
   }
@@ -111,8 +141,9 @@ function buygen3() {
     gen3Cost *= 1.2;
     gen3Cost = Math.round(gen3Cost);
     document.getElementById("gen3price").innerText = gen3Cost;
-    pointPerSecond += 7;
+    pointPerSecond += gen3gain;
     document.getElementById("showpsec").innerText = pointPerSecond;
+    document.getElementById("gen3add").innerText = gen3gain;
   } else {
     alert("You don't have enough points!")
   }
